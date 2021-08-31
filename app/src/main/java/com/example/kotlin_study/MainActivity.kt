bbb8148027d78fe5f0d34778cab89f5d0a6d84bc
dabinKim-0318 <alalekqls22@naver.com>
2021-08-31 21:11:06 +0900

    0831

--- app/src/main/java/com/example/kotlin_study/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_study/MainActivity.kt
@@ -6,6 +6,6 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.scrollview)
+        setContentView(R.layout.all_test_answer)
     }
 }
