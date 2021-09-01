39ff13c4f89bc68832a0a8f495d1f0ca0b36f1d0
dabinKim-0318 <alalekqls22@naver.com>
2021-09-01 19:17:40 +0900

    0901라이프사이클

--- app/src/main/java/com/example/kotlin_study/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_study/MainActivity.kt
@@ -2,6 +2,10 @@ package com.example.kotlin_study
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+//임포트는 내가 해당 파일에서 가져다 쓸 것 정리하는거고
+//이 메인액티비티는 기본으로 상단 2개의 임포트가 디폴트임
+//MainActivity는 AppCompatActivity라는 클래스를 상속받았음
+//MainActivity에서는 디폴트로 onCreate매서드가 선언되어있음
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
