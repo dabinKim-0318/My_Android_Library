a0d21b87a902d85c50bafd68128bc3c4e407990e
dabinKim-0318 <alalekqls22@naver.com>
2021-09-09 22:54:47 +0900

    0909_start

---
+++ app/src/main/java/com/example/kotlin_study/Android/RatingBar.kt
@@ -0,0 +1,12 @@
+package com.example.kotlin_study.Android
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.kotlin_study.R
+
+class RatingBar : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rating_bar)
+    }
+}
