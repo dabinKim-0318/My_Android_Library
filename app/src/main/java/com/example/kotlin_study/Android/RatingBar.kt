034f3f000d78a999b75adf8308b82e3306fc6491
dabinKim-0318 <alalekqls22@naver.com>
2021-09-10 23:12:59 +0900

    0910_star

--- app/src/main/java/com/example/kotlin_study/Android/RatingBar.kt
+++ app/src/main/java/com/example/kotlin_study/Android/RatingBar.kt
@@ -3,10 +3,17 @@ package com.example.kotlin_study.Android
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.example.kotlin_study.R
+import kotlinx.android.synthetic.main.activity_rating_bar.*
 
 class RatingBar : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_rating_bar)
+
+        ratingbarStlye.setOnRatingBarChangeListener{ ratingBar,rating,fromUser->
+
+
+        }
+
     }
 }
