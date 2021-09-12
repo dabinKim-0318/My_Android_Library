9aa82d65c6a2cbad187a2f1b7a4af357fa1d1d18
dabinKim-0318 <alalekqls22@naver.com>
2021-09-12 23:13:11 +0900

    0912_AddView_HomeWork

---
+++ app/src/main/java/com/example/kotlin_study/Android/CallingList.kt
@@ -0,0 +1,12 @@
+package com.example.kotlin_study.Android
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.kotlin_study.R
+
+class CallingList : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calling_list)
+    }
+}
