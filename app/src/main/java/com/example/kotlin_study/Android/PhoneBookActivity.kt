b7db4872ce26a774acb29406589e38f7f0f05104
dabinKim-0318 <alalekqls22@naver.com>
2021-09-13 21:48:39 +0900

    0913_Call_List

--- app/src/main/java/com/example/kotlin_study/Android/PhoneBookActivity.kt
+++ app/src/main/java/com/example/kotlin_study/Android/PhoneBookActivity.kt
@@ -8,18 +8,24 @@ import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.kotlin_study.R
+class PhoneBook() {
+    val personList = ArrayList<Person>()
+    fun addPerson(person: Person) {
+        personList.add(person)
+    }
+}
+class Person(val name: String, val number: String) {}
+
 
 class PhoneBookActivity : AppCompatActivity() {
+    //Activity가 시작되고 onCreate의 범위가 어디까지인가 잘 생각!!!=무엇을 호출시킬 것인가?
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_phone_book)
 
         val phoneBook = createFakePhoneBook(30)
         createPhoneBookList(phoneBook)
-
-
-        phoneBook.personList
-    }
+  }
 
     fun createFakePhoneBook(faskNumber: Int = 10, phoneBook: PhoneBook = PhoneBook()): PhoneBook {
         for (i in 0 until faskNumber) {
@@ -32,17 +38,20 @@ class PhoneBookActivity : AppCompatActivity() {
     fun createPhoneBookList(phoneBook: PhoneBook) {
         val layoutInflater = LayoutInflater.from(this@PhoneBookActivity)
         val container = findViewById<LinearLayout>(R.id.phonebook_list_container)
+
         for (i in 0 until phoneBook.personList.size) {
             val view = layoutInflater.inflate(R.layout.phonebook_item, null, false)
             val personNameView = view.findViewById<TextView>(R.id.person_name)
             personNameView.setText(phoneBook.personList.get(i).name)
 
+            //addSetOnClickListener을 직접 정의해서 각 객체가 리스트로 만들어질때 setOnClickListener호출 대기 상태가 되도록
             addSetOnClickListener(phoneBook.personList.get(i), view)
             container.addView(view)
         }
     }
 
     fun addSetOnClickListener(person: Person, view: View) {
+        //각 객체의 setOnClickListener가 호출될때 intent로 정보 전달하면서 상세 Activity 실행
         view.setOnClickListener {
             val intent = Intent(this@PhoneBookActivity, PhoneBook_detail_Activity::class.java)
             intent.putExtra("name", person.name)
@@ -53,11 +62,3 @@ class PhoneBookActivity : AppCompatActivity() {
 
 }
 
-class PhoneBook() {
-    val personList = ArrayList<Person>()
-    fun addPerson(person: Person) {
-        personList.add(person)
-    }
-}
-
-class Person(val name: String, val number: String) {}
