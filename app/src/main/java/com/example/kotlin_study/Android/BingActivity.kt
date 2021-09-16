6465eff57c70888d28c10c047758c17d2e01906d
dabinKim-0318 <alalekqls22@naver.com>
2021-09-16 23:35:47 +0900

    0916_Bing

---
+++ app/src/main/java/com/example/kotlin_study/Android/BingActivity.kt
@@ -0,0 +1,12 @@
+package com.example.kotlin_study.Android
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.kotlin_study.R
+
+class BingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bing)
+    }
+}
