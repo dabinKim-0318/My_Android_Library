28a18fa099a7fad1b42852392d0cf5636245b9c2
dabinKim-0318 <alalekqls22@naver.com>
2021-09-23 23:10:19 +0900

    0923_recycle_view

--- app/src/main/java/com/example/kotlin_study/Android/CallingList2.kt
+++ app/src/main/java/com/example/kotlin_study/Android/CallingList2.kt
@@ -1,7 +1,6 @@
 package com.example.kotlin_study.Android
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,35 +10,91 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.kotlin_study.R
 import kotlinx.android.synthetic.main.activity_calling_list2.*
-import kotlinx.android.synthetic.main.activity_recycle_view.*
+import kotlinx.android.synthetic.main.person_list.view.*
 
-class callList(val name: String, val number: String) {
+//
+//class callList(val name: String, val number: String) {
+//
+//}
+//
+//class CallingList2 : AppCompatActivity() {
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_calling_list2)
+//
+//        //데이터 리스트 만들기
+//        val phonelist = ArrayList<callList>()
+//        for (item in 0..20) {
+//            phonelist.add(callList(" $item ", "010- $item -8437"))
+//
+//        }
+//
+//        val adapter = RecyclerViewAdapter2(phonelist, LayoutInflater.from(this@CallingList2))
+//        recyclerView.adapter = adapter
+//        recyclerView.layoutManager = LinearLayoutManager(this@CallingList2)
+//        //recyclerView.layoutManager = GridLayoutManager(this@RecycleViewActivity,2)
+//
+//
+//    }
+//}
+//
+//class RecyclerViewAdapter2(
+//    val phonelist: ArrayList<callList>,
+//    val inflater: LayoutInflater
+//) : RecyclerView.Adapter<RecyclerViewAdapter2.ViewHolder2>() {
+//
+//    inner class ViewHolder2(itemView: View) : RecyclerView.ViewHolder(itemView) {
+//        var name: TextView
+//        var story: TextView
+//
+//        init {
+//            name = itemView.findViewById(R.id.name)
+//            story = itemView.findViewById(R.id.story)
+//            itemView.setOnClickListener {
+//                val positoon:Int=adapterPosition
+//                val number=phonelist.get(positoon).number
+//                Log.d("number",number)
+//            }
+//        }
+//
+//    }
+//
+//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):ViewHolder2 {
+//        val view = inflater.inflate(R.layout.person_list, parent, false)
+//        return ViewHolder2(view)
+//    }
+//
+//    override fun getItemCount(): Int {
+//        return phonelist.size
+//    }
+//
+//    override fun onBindViewHolder(holder: ViewHolder2, position: Int) {
+//        holder.name.setText(phonelist.get(position).name)
+//        holder.story.setText(phonelist.get(position).number)
+//    }
+//
+//}
 
-}
+class callist(val name: String, val number: String) {}
 
 class CallingList2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_calling_list2)
 
-        //데이터 리스트 만들기
-        val phonelist = ArrayList<callList>()
+        val phonelist = ArrayList<callist>()
         for (item in 0..20) {
-            phonelist.add(callList(" $item ", "010- $item -8437"))
-
+            phonelist.add(callist(" $item ", "010- $item "))
         }
 
-        val adapter = RecyclerViewAdapter2(phonelist, LayoutInflater.from(this@CallingList2))
-        recyclerView.adapter = adapter
-        recyclerView.layoutManager = LinearLayoutManager(this@CallingList2)
-        //recyclerView.layoutManager = GridLayoutManager(this@RecycleViewActivity,2)
-
-
+       val adapter= RecyclerViewAdapter2(phonelist, LayoutInflater.from(this@CallingList2))
+        recyclerView.adapter=adapter
+        recyclerView.layoutManager=LinearLayoutManager(this)
     }
 }
 
 class RecyclerViewAdapter2(
-    val phonelist: ArrayList<callList>,
+    val phonelist: ArrayList<callist>,
     val inflater: LayoutInflater
 ) : RecyclerView.Adapter<RecyclerViewAdapter2.ViewHolder2>() {
 
@@ -48,18 +103,15 @@ class RecyclerViewAdapter2(
         var story: TextView
 
         init {
-            name = itemView.findViewById(R.id.name)
-            story = itemView.findViewById(R.id.story)
+            name = itemView.name
+            story = itemView.story
             itemView.setOnClickListener {
-                val positoon:Int=adapterPosition
-                val number=phonelist.get(positoon).number
-                Log.d("number",number)
+
             }
         }
-
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int):ViewHolder2 {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder2 {
         val view = inflater.inflate(R.layout.person_list, parent, false)
         return ViewHolder2(view)
     }
@@ -69,8 +121,13 @@ class RecyclerViewAdapter2(
     }
 
     override fun onBindViewHolder(holder: ViewHolder2, position: Int) {
-        holder.name.setText(phonelist.get(position).name)
-        holder.story.setText(phonelist.get(position).number)
+       holder.name.setText(phonelist.get(position).name)
+       holder.story.setText(phonelist.get(position).number)
+
     }
 
+
 }
+
+
+
