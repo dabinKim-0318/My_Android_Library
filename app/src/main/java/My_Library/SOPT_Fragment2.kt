523af73f16efea654e05ff58a62c8a141e5e781b
dabinKim-0318 <alalekqls22@naver.com>
2021-10-17 22:06:44 +0900

    1017_Fragment

--- app/src/main/java/My_Library/SOPT_Fragment2.kt
+++ app/src/main/java/My_Library/SOPT_Fragment2.kt
@@ -9,7 +9,7 @@ import com.example.kotlin_study.databinding.FragmentSopt2Binding
 
 
 class SOPT_Fragment2 : Fragment() {
-    private var _binding: FragmentSopt2Binding? = null
+    private var _binding: FragmentSopt2Binding? = null  //binding 파일 이름이 Activity로 시작하지 않을 수 있네
     private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
