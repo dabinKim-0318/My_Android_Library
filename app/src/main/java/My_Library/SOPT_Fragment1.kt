eeff1729cf6c9a7820484d428dbfd46c794732f5
dabinKim-0318 <alalekqls22@naver.com>
2021-10-21 21:29:11 +0900

    1021_Fragment

--- app/src/main/java/My_Library/SOPT_Fragment1.kt
+++ app/src/main/java/My_Library/SOPT_Fragment1.kt
@@ -5,22 +5,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.example.kotlin_study.databinding.ActivitySoptFragmentBinding
+import com.example.kotlin_study.databinding.FragmentSOPTBinding
 
 class SOPT_Fragment1 : Fragment() {
-    private var _binding: ActivitySoptFragmentBinding? = null
+    private var _binding: FragmentSOPTBinding? = null
     private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        _binding=ActivitySoptFragmentBinding.inflate(layoutInflater,container,false)
+        _binding = FragmentSOPTBinding.inflate(layoutInflater, container, false)
         return binding.root
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        _binding=null
+        _binding = null
     }
 }
