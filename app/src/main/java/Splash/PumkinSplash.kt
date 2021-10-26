f5c5d4a80dd41565c1adc63ca6ba3fcc09265d55
dabinKim-0318 <alalekqls22@naver.com>
2021-10-26 16:23:18 +0900

    1026_Splash

--- app/src/main/java/Splash/PumkinSplash.kt
+++ app/src/main/java/Splash/PumkinSplash.kt
@@ -5,12 +5,14 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
-import com.example.kotlin_study.R
+import com.example.kotlin_study.databinding.ActivityPumkinSplash2Binding
 
 class PumkinSplash : AppCompatActivity() {
+    private lateinit var binding: ActivityPumkinSplash2Binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_pumkin_splash2)
+        binding = ActivityPumkinSplash2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         startLoading()
     }
