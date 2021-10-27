eb4be85642fa987da285fbb9436f4554bcaa1144
dabinKim-0318 <alalekqls22@naver.com>
2021-10-27 11:54:22 +0900

    Apply .gitignore

---
+++ app/src/main/java/com/example/kotlin_study/Android/GithubServiceImplementation.kt
@@ -0,0 +1,20 @@
+package com.example.kotlin_study.Android
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object GithubServiceImplementation {
+
+    //BASE_URL을 private const 변수로 저장해둠
+    private const val BASE_URL = "https://api.github.com"
+
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        // 이 부분 추가하기
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    //GithubService는 Request메소드를 작성해둔 인터페이스 이름임
+    val service = retrofit.create(GithubService::class.java)
+
+}
