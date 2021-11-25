366df3926e53227a2ea268b61ff36815ac5a4cd6
DaBin <84564695+dabinKim-0318@users.noreply.github.com>
2021-11-25 13:24:40 +0900

    [ADD] Coroutine

--- app/src/main/java/Kotlin/coroutine.kt
+++ app/src/main/java/Kotlin/coroutine.kt
@@ -1,8 +1,23 @@
 package Kotlin
 
 
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 
-fun main() = runBlocking {
-
+fun main() = runBlocking<Unit> {
+    // launch a coroutine to process some kind of incoming request
+    val request = CoroutineScope(Dispatchers.Default).launch {
+        repeat(3) { i -> // launch a few children jobs
+            launch {
+                delay((i + 1) * 200L) // variable delay 200ms, 400ms, 600ms
+                println("Coroutine $i is done")
+            }
+        }
+        println("request: I'm done and I don't explicitly join my children that are still active")
+    }
+    println("Now processing of the request is complete")
 }
+/*Now processing of the request is complete
+request: I'm done and I don't explicitly join my children that are still active
+Coroutine 0 is done
+Coroutine 1 is done
+Coroutine 2 is done*/
