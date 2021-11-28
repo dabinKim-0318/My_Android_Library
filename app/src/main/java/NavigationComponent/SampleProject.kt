7069d698208af02a0aee4cba4882bdae87a4358f
DaBin <84564695+dabinKim-0318@users.noreply.github.com>
2021-11-28 20:46:19 +0900

    [ADD] Navigation component

---
+++ app/src/main/java/NavigationComponent/SampleProject.kt
@@ -0,0 +1,12 @@
+package NavigationComponent
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.kotlin_study.R
+
+class SampleProject : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sample_project)
+    }
+}
