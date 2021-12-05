6517e3ad8b3684302a49eaf2d4e245ff8801bff5
DaBin <84564695+dabinKim-0318@users.noreply.github.com>
2021-12-05 23:24:04 +0900

    [ADD] Util

--- app/src/main/java/Util/util.kt
+++ app/src/main/java/Util/util.kt
@@ -1,9 +1,14 @@
 package Util
 
 import android.content.Context
+import android.view.View
 import android.widget.Toast
+import kotlin.math.roundToInt
 
 fun Context.shortToast(messege: String) {
-    Toast.makeText(this,messege,Toast.LENGTH_SHORT).show()
+    Toast.makeText(this, messege, Toast.LENGTH_SHORT).show()
 }
 
+fun View.px(dp: Int) {
+    (dp * resources.displayMetrics.density).roundToInt()
+}
