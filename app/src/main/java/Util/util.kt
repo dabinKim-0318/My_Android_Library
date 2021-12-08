f1a5bb15831d47cce92c14a5dfa174de7e9229c0
DaBin <84564695+dabinKim-0318@users.noreply.github.com>
2021-12-08 21:48:00 +0900

    [ADD] Util

--- app/src/main/java/Util/util.kt
+++ app/src/main/java/Util/util.kt
@@ -9,6 +9,7 @@ fun Context.shortToast(messege: String) {
     Toast.makeText(this, messege, Toast.LENGTH_SHORT).show()
 }
 
+//사용이 안됨
 fun View.px(dp: Int) {
     (dp * resources.displayMetrics.density).roundToInt()
 }
